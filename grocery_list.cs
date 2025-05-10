2f90c63e9f5ae4889a6b3c5c8910ee2eb0a9ac9e
Saad-hassan78 <saadhassan9577@gmail.com>
2025-05-10 14:44:10 +0500

    Create grocery_list.cs

---
+++ grocery_list.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+class Program {
+    static void Main() {
+        List<string> groceries = new List<string>() {
+            "Milk",
+            "Bread",
+            "Eggs",
+            "Apples",
+            "Rice"
+        };
+
+        foreach (var item in groceries) {
+            Console.WriteLine(item);
+        }
+    }
+}
